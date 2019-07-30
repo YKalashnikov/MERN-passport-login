17b33b7506d3b7f85bfc6e2733ddc55f43a0f599
YKalashnikov <uk2709@gmail.com>
2019-07-30 10:51:52 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const index = require("./routes/index");
+const users = require("./routes/users");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const flash = require("connect-flash");
+const session = require("express-session");
+
+mongoose
+  .connect(db, { useNewUrParser: true })
+  .then(() => console.log("Mongo is connected"))
+  .catch(err => console.log(err));
+
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "Secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
+});
+app.use("/", index);
+app.use("/users", users);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ routes/users.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill in all fields" });
+  }
+  if (password != password2) {
+    errors.push({ msg: "Password does not match" });
+  }
+  if (password.length < 5) {
+    errors.push({ msg: "Please make password more then 5 characters" });
+  }
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const user = new User({
+          name,
+          email,
+          password,
+          password2
+        });
+        
+         bcrypt.genSalt(10, (err, salt)=>{
+             bcrypt.hash(user.password, salt, (err, hash)=> {
+                 if(err) throw err;
+                 user.password = hash;
+                 user.save()
+                 .then(user => {
+                    req.flash(
+                        'success_msg',
+                        'You are now registered and can log in'
+                      );
+                    res.redirect('/users/login')
+                 })
+                 .catch(err => console.log(err))
+
+             })
+         })
+
+        console.log(hashedPassword);
+        res.send({ user: email });
+      }
+    });
+  }
+});
+module.exports = router;
